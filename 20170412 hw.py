8c75c15294a63ef937bf72a556e18e8e3b6a3884
subin9757 <subin9757@naver.com>
2017-04-12 16:01:34 +0900

    Add files via upload

---
+++ 20170412 hw.py
@@ -0,0 +1,38 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> wn = turtle.Screen()
+>>> t1 = turtle.Turtle()
+>>> tl.fw(00)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    tl.fw(00)
+NameError: name 'tl' is not defined
+>>> tl.fd(100)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    tl.fd(100)
+NameError: name 'tl' is not defined
+>>> t1.fd(100)
+>>> t1.rt(90)
+>>> t1.fd(100)
+>>> wn.addshape(C:\\Users\\lsb.gif")
+	    
+SyntaxError: invalid syntax
+>>> wn.addshape("C:\\Users\\lsb.gif")
+>>> t1.shape("C:\\Users\\lsb.gif")
+>>> tl.fd(500)
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    tl.fd(500)
+NameError: name 'tl' is not defined
+>>> t1.fd(500)
+>>> t1.rt(90)
+>>> t1.fd(500)
+>>> t1.rt(90)
+>>> t1.fd(500)
+>>> t1.rt(90)
+>>> t1.fd(100)
+>>> t1.fd(100)
+>>> t1.fd(50)
+>>> 
