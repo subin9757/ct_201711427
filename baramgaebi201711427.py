9233a1b5c30d5d4c4327a46a93c22f13737da2db
subin9757 <subin9757@naver.com>
2017-05-08 17:33:12 +0900

    Add files via upload

---
+++ baramgaebi201711427.py
@@ -0,0 +1,23 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def giyuk(size):
+	t1.fd(size)
+	t1.rt(90)
+	t1.fd(size)
+	t1.penup()
+	pos1=(0,0)
+	t1.goto(pos1)
+	t1.rt(45)
+	t1.pendown()
+
+
+giyuk(50)
+giyuk(50)
+giyuk(50)
+giyuk(50)
+giyuk(50)
+giyuk(50)
+giyuk(50)
+giyuk(50)
+giyuk(50)	
