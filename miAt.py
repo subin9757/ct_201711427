42978afdf6dedd9aa72e2d0d97e2d05a6405fb56
subin9757 <subin9757@naver.com>
2017-05-22 17:06:07 +0900

    Add files via upload

---
+++ miAt.py
@@ -0,0 +1,33 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def giyukAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.fd(size)
+    t1.rt(90)
+    t1.fd(size)
+
+
+
+def nieunAt(x,y,size):
+    t1.penup()
+    t1.goto(x,y)
+    t1.pendown()
+    t1.rt(90)
+    t1.fd(size)
+    t1.lt(90)
+    t1.fd(size)
+
+
+def miAt(x,y,size):
+    giyukAt(x,y,size)
+    t1.penup()
+    t1.goto(x,y)
+    t1.lt(90)
+    t1.pendown()
+    nieunAt(x,y,size)
+
+miAt(-100,100,100)
