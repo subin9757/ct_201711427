f36d8f8011ac2c517fe5e11c09951cb099c9fe67
subin9757 <subin9757@naver.com>
2017-05-29 16:15:06 +0900

    Add files via upload

---
+++ draw20170529.py
@@ -0,0 +1,57 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+
+def drawOAt(size,at):
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+    t1.fd(100)
+    t1.write(t1.pos())
+    t1.lt(72)
+    t1.fd(100)
+    t1.write(t1.pos())
+
+drawOAt(100,0)
+
+# triangle
+def drawtringleAt(size,at):
+    t1.penup()
+    t1.goto(300,0)
+    t1.pendown()
+    t1.forward(100)
+    t1.write(t1.pos())
+    t1.left(120)
+    t1.forward(100)
+    t1.write(t1.pos())
+    t1.left(120)
+    t1.forward(100)
+    t1.write(t1.pos())
+    t1.left(120)
+    
+drawtringleAt(100,-100)
+
+def drawstarAt(size,at):
+    t1.penup()
+    t1.goto(-300,0)
+    t1.setheading(0)
+    t1.pendown()
+    t1.write(t1.pos())
+    t1.fd(size)
+    t1.lt(144)
+    t1.fd(size)
+    t1.lt(144)
+    t1.fd(size)
+    t1.lt(144)
+    t1.fd(size)
+    t1.lt(144)
+    t1.fd(size)
+    t1.lt(144)
+
+drawstarAt(100,100)
